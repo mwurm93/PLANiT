95a2345840018a1906f69033e4910c761064db68
mwurm93 <michaelwurm@MICHAELs-MacBook-Pro.local>
2016-10-11 16:29:12 -0400

    Transition b/w login/signup and triplist

---
+++ PLANiT/BlurSignUp2TripList.swift
@@ -0,0 +1,35 @@
+//
+//  BlurSignUp2TripList.swift
+//  PLANiT
+//
+//  Created by MICHAEL WURM on 10/11/16.
+//  Copyright © 2016 MICHAEL WURM. All rights reserved.
+//
+
+import UIKit
+
+class BlurSignUp2TripList: UIStoryboardSegue {
+    
+    override func perform() {
+        
+        //set the ViewControllers for the animation
+        
+        let sourceView = self.source.view as UIView!
+        let destinationView = self.destination.view as UIView!
+        
+        
+        let window = UIApplication.shared.delegate?.window!
+        //set the destination View center
+        //destinationView?.center = CGPoint(x: (sourceView?.center.x)!, y: (sourceView?.center.y)! - (destinationView?.center.y)!)
+        
+        // the Views must be in the Window hierarchy, so insert as a subview the destionation above the source
+        window?.insertSubview(destinationView!, aboveSubview: sourceView!)
+        
+        //create UIAnimation- change the views's position when present it
+        UIView.transition(from: sourceView!, to: destinationView!, duration: 1.0 , options: UIViewAnimationOptions.transitionCrossDissolve, completion: {
+                (value: Bool) in
+                self.source.navigationController?.pushViewController(self.destination, animated: false)
+                
+        })
+    }
+}
