07ad65b6e265181602ca864af3c8fe7d1f448f95
mwurm93 <michaelwurm@MICHAELs-MacBook-Pro.local>
2016-10-11 20:36:51 -0400

    more..

---
+++ PLANiT/TripListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TripList.swift
+//  PLANiT
+//
+//  Created by MICHAEL WURM on 10/11/16.
+//  Copyright © 2016 MICHAEL WURM. All rights reserved.
+//
+
+import UIKit
+
+class TripListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
