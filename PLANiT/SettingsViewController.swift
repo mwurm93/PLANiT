417c9bc65ceefad501797ba52a6d0243fff0c776
mwurm93 <michaelwurm@MICHAELs-MacBook-Pro.local>
2016-10-20 18:24:28 -0400

    nNot

--- PLANiT/SettingsViewController.swift
+++ PLANiT/SettingsViewController.swift
@@ -8,25 +8,104 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Outlets
     @IBOutlet weak var nameHeaderForSettings: UILabel!
+    @IBOutlet weak var emailAddressSettingsField: UITextField!
+    @IBOutlet weak var passwordSettingsField: UITextField!
+    @IBOutlet weak var firstNameSettingsField: UITextField!
+    @IBOutlet weak var lastNameSettingsField: UITextField!
+    @IBOutlet weak var genderSettingsField: UITextField!
+    @IBOutlet weak var phoneSettingsField: UITextField!
+    @IBOutlet weak var homeAirportSettingsField: UITextField!
+    @IBOutlet weak var passportNumberSettingsField: UITextField!
+    @IBOutlet weak var knownTravelerNumberSettingsField: UITextField!
+    @IBOutlet weak var redressNumberSettingsField: UITextField!
+    @IBOutlet weak var birthdateSettingsField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.emailAddressSettingsField.delegate = self
+        self.passwordSettingsField.delegate = self
+        self.firstNameSettingsField.delegate = self
+        self.lastNameSettingsField.delegate = self
+        self.genderSettingsField.delegate = self
+        self.phoneSettingsField.delegate = self
+        self.homeAirportSettingsField.delegate = self
+        self.passportNumberSettingsField.delegate = self
+        self.knownTravelerNumberSettingsField.delegate = self
+        self.redressNumberSettingsField.delegate = self
+        self.birthdateSettingsField.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool)
     {
         //Load the values from our shared data container singleton
         let firstNameValue = DataContainerSingleton.sharedDataContainer.firstName ?? ""
+        let lastNameValue = DataContainerSingleton.sharedDataContainer.lastName ?? ""
+        let emailAddressValue = DataContainerSingleton.sharedDataContainer.emailAddress ?? ""
+        let passwordValue = DataContainerSingleton.sharedDataContainer.password ?? ""
+        let genderValue = DataContainerSingleton.sharedDataContainer.gender ?? ""
+        let phoneValue = DataContainerSingleton.sharedDataContainer.phone ?? ""
+        let homeAirportValue = DataContainerSingleton.sharedDataContainer.homeAirport ?? ""
+        let passportNumberValue = DataContainerSingleton.sharedDataContainer.passportNumber ?? ""
+        let knownTravelerNumberValue = DataContainerSingleton.sharedDataContainer.knownTravelerNumber ?? ""
+        let redressNumberValue = DataContainerSingleton.sharedDataContainer.redressNumber ?? ""
+        let birthdateValue = DataContainerSingleton.sharedDataContainer.birthdate ?? ""
         
         //Install the value into the text field.
         self.nameHeaderForSettings.text =  "\(firstNameValue)'s Profile"
+        self.firstNameSettingsField.text =  "\(firstNameValue)"
 
+        self.lastNameSettingsField.text =  "\(lastNameValue)"
+        self.emailAddressSettingsField.text =  "\(emailAddressValue)"
+        self.passwordSettingsField.text =  "\(passwordValue)"
+        self.genderSettingsField.text =  "\(genderValue)"
+        self.phoneSettingsField.text =  "\(phoneValue)"
+        self.homeAirportSettingsField.text =  "\(homeAirportValue)"
+        self.passportNumberSettingsField.text =  "\(passportNumberValue)"
+        self.knownTravelerNumberSettingsField.text =  "\(knownTravelerNumberValue)"
+        self.redressNumberSettingsField.text =  "\(redressNumberValue)"
+        self.birthdateSettingsField.text =  "\(birthdateValue)"
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField:  UITextField) -> Bool {
+        // Hide the keyboard.
+        firstNameSettingsField.resignFirstResponder()
+        lastNameSettingsField.resignFirstResponder()
+        emailAddressSettingsField.resignFirstResponder()
+        passwordSettingsField.resignFirstResponder()
+        genderSettingsField.resignFirstResponder()
+        phoneSettingsField.resignFirstResponder()
+        homeAirportSettingsField.resignFirstResponder()
+        passportNumberSettingsField.resignFirstResponder()
+        knownTravelerNumberSettingsField.resignFirstResponder()
+        redressNumberSettingsField.resignFirstResponder()
+        birthdateSettingsField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        DataContainerSingleton.sharedDataContainer.firstName = firstNameSettingsField.text
+        DataContainerSingleton.sharedDataContainer.lastName = lastNameSettingsField.text
+        DataContainerSingleton.sharedDataContainer.emailAddress = emailAddressSettingsField.text
+        DataContainerSingleton.sharedDataContainer.password = passwordSettingsField.text
+        DataContainerSingleton.sharedDataContainer.gender = genderSettingsField.text
+        DataContainerSingleton.sharedDataContainer.phone = phoneSettingsField.text
+        DataContainerSingleton.sharedDataContainer.homeAirport = homeAirportSettingsField.text
+        DataContainerSingleton.sharedDataContainer.passportNumber = passportNumberSettingsField.text
+        DataContainerSingleton.sharedDataContainer.knownTravelerNumber = knownTravelerNumberSettingsField.text
+        DataContainerSingleton.sharedDataContainer.redressNumber = redressNumberSettingsField.text
+        DataContainerSingleton.sharedDataContainer.birthdate = birthdateSettingsField.text
+        return true
+    }
+
 }
--- PLANiT/TravelerDetailsViewController.swift
+++ PLANiT/TravelerDetailsViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class TravelerDetails: UIViewController {
+class TravelerDetails: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var homeAirport: UITextField!
+    @IBOutlet weak var gender: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.homeAirport.delegate = self
+        self.gender.delegate = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        //Load the values from our shared data container singleton
+        let genderValue = DataContainerSingleton.sharedDataContainer.gender ?? ""
+        let homeAirportValue = DataContainerSingleton.sharedDataContainer.homeAirport ?? ""
+        
+        //Install the value into the text field.
+        self.gender.text =  "\(genderValue)"
+        self.homeAirport.text =  "\(homeAirportValue)"
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +38,18 @@ class TravelerDetails: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField:  UITextField) -> Bool {
+        // Hide the keyboard.
+        homeAirport.resignFirstResponder()
+        gender.resignFirstResponder()
+        return true
     }
-    */
-
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        DataContainerSingleton.sharedDataContainer.homeAirport = homeAirport.text
+        DataContainerSingleton.sharedDataContainer.gender = gender.text
+        return true
+    }
 }
