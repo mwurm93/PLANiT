1e274937ba75b8b9b494e0472540497ec4d6f234
mwurm93 <michaelwurm@michaels-macbook-pro.local>
2016-12-17 20:09:58 -0800

    xxxxxxx

---
+++ PLANiT/rankingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  rankingViewController.swift
+//  PLANiT
+//
+//  Created by MICHAEL WURM on 12/17/16.
+//  Copyright © 2016 MICHAEL WURM. All rights reserved.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController {
+
+// MARK: Outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
