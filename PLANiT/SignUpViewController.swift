9d7120af81e65fe88230146fe10d5667fdd8d483
mwurm93 <michaelwurm@michaels-mbp.lan>
2016-12-20 19:48:39 -0500

    uneventful changes

--- PLANiT/SignUpViewController.swift
+++ PLANiT/SignUpViewController.swift
@@ -16,16 +16,18 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailAddress: UITextField!
     @IBOutlet weak var password: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.firstName.delegate = self
         self.lastName.delegate = self
         self.emailAddress.delegate = self
         self.password.delegate = self
-        apollo.fetch(query: GetTripQuery(id: "VHJpcDox")) { (result, error) in
-            guard let data = result?.data else { return }
-            self.emailAddress.text = data.getTrip?.name;
-        }
+        
+        // apollo.fetch(query: GetTripQuery(id: "VHJpcDox")) { (result, error) in
+        //    guard let data = result?.data else { return }
+        //    self.emailAddress.text = data.getTrip?.name;
+        // }
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,11 +46,22 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
         DataContainerSingleton.sharedDataContainer.firstName = firstName.text
         DataContainerSingleton.sharedDataContainer.lastName = lastName.text
         DataContainerSingleton.sharedDataContainer.emailAddress = emailAddress.text
         DataContainerSingleton.sharedDataContainer.password = password.text
         return true
     }
+    
+    // MARK: Actions
+    @IBAction func CreateNewUserButtonPressed(_ sender: Any) {
+        
+        apollo.perform(mutation: CreateAUserMutation(newUser: CreateUserInput(username: self.emailAddress.text!,password: self.password.text!))) { (result, error) in
+
+        }
+        
+    }
+    
     
 }
