970d0534262a197cf9ccd8d62c68327b8b839fa4
mwurm93 <michaelwurm@MICHAELs-MacBook-Pro.local>
2017-01-07 15:18:11 -0500

    fad

--- PLANiT/ActivitiesViewController.swift
+++ PLANiT/ActivitiesViewController.swift
@@ -20,10 +20,15 @@ class ActivitiesViewController: UIViewController, UICollectionViewDataSource, UI
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.allowsMultipleSelection = true
+        
+        // Call collection initializer
         initActivityItems()
         collectionView.reloadData()
 
+        //update aesthetics
         collectionView.layer.cornerRadius = 5
+        collectionView.layer.backgroundColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 0).cgColor
         userToggles.layer.cornerRadius = 5
         
         //Load the values from our shared data container singleton
@@ -34,6 +39,29 @@ class ActivitiesViewController: UIViewController, UICollectionViewDataSource, UI
         }
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        // Update cell border color to blue if saved as a selected activity
+        let selectedActivity_0 = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "selected_activity_0") as? String
+        let selectedActivity_1 = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "selected_activity_1") as? String
+        let selectedActivity_2 = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "selected_activity_2") as? String
+        let selectedActivity_3 = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "selected_activity_3") as? String
+        
+        let visibleCellIndices = self.collectionView.indexPathsForVisibleItems
+        for visibleCellIndex in visibleCellIndices {
+            let visibleCell = collectionView.cellForItem(at: visibleCellIndex) as! ActivitiesCollectionViewCell
+            if visibleCell.activityLabel.text == selectedActivity_0 || visibleCell.activityLabel.text == selectedActivity_1  || visibleCell.activityLabel.text == selectedActivity_2  || visibleCell.activityLabel.text == selectedActivity_3 {
+                visibleCell.layer.borderColor = UIColor.blue.cgColor
+                collectionView.selectItem(at: visibleCellIndex, animated: true, scrollPosition: .top)
+            }
+            else {
+                visibleCell.layer.borderColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1).cgColor
+                collectionView.deselectItem(at: visibleCellIndex, animated: true)
+
+            }
+        }
+        
+    }
+    
     fileprivate func initActivityItems() {
         
         var items = [ActivityItem]()
@@ -46,54 +74,146 @@ class ActivitiesViewController: UIViewController, UICollectionViewDataSource, UI
             items.append(activityItem)
         }
         activityItems = items
+        
     }
     
     // MARK: - UICollectionViewDataSource
-    
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return activityItems.count
     }
-    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
+        collectionView.allowsMultipleSelection = true
+
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "activitiesViewPrototypeCell", for: indexPath) as! ActivitiesCollectionViewCell
         
         cell.setActivityItem(activityItems[indexPath.row])
-
-        // Set border color
-            cell.layer.borderWidth = 4.0
-            cell.layer.borderColor = UIColor.lightGray.cgColor
-            cell.layer.cornerRadius = 10
+        cell.layer.borderWidth = 4.0
+        cell.layer.borderColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1).cgColor
+        cell.layer.cornerRadius = 10
         
         return cell
     }
     
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
     // MARK: - UICollectionViewDelegate
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    // Item DEselected: update border color and save data when
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         
-        let selectedCell = collectionView.cellForItem(at: indexPath)
-        if selectedCell?.layer.borderColor == UIColor.lightGray.cgColor
-            {
-            selectedCell?.layer.borderColor = UIColor.blue.cgColor
-            }
-        else if selectedCell?.layer.borderColor == UIColor.blue.cgColor
-        {
-            selectedCell?.layer.borderColor = UIColor.lightGray.cgColor
+        // Change border color to grey
+        let deSelectedCell = collectionView.cellForItem(at: indexPath)
+        deSelectedCell!.layer.borderColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1).cgColor
+        
+        // Create array of selected activities
+        var selectedActivities = [String?]()
+        let indexPaths = self.collectionView!.indexPathsForSelectedItems
+        for indexItem in indexPaths! {
+            let currentCell = collectionView.cellForItem(at: indexItem)! as! ActivitiesCollectionViewCell
+            let selectedActivity = currentCell.activityLabel.text
+            selectedActivities.append(selectedActivity)
         }
         
-        // Create alert with index
-//        let alert = UIAlertController(title: "didSelectItemAtIndexPath:", message: "Indexpath = \(indexPath)", preferredStyle: .alert)
-//        
-//        let alertAction = UIAlertAction(title: "Dismiss", style: .destructive, handler: nil)
-//        alert.addAction(alertAction)
-//        
-//        self.present(alert, animated: true, completion: nil)
+        // Save array of selected activities to trip data model
+        var existing_trips = DataContainerSingleton.sharedDataContainer.usertrippreferences
+        let currentTripIndex = DataContainerSingleton.sharedDataContainer.currenttrip!
+        let tripNameValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "trip_name") as? String
+        let multipleDestionationsValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "multiple_destinations") as? String
+        let travelingInternationalValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "traveling_international") as? String
+        let suggestDestinationControlValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "suggest_destination_control") as? String
+        let suggestedDestinationValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "suggested_destination") as? String
+        let budgetValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "budget") as? String
+        
+        if selectedActivities.count == 0 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": nil, "selected_activity_1": nil, "selected_activity_2": nil, "selected_activity_3": nil]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count == 1 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": selectedActivities[0], "selected_activity_1": nil, "selected_activity_2": nil, "selected_activity_3": nil]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count == 2 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": selectedActivities[0], "selected_activity_1": selectedActivities[1], "selected_activity_2": nil, "selected_activity_3": nil]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count == 3 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": selectedActivities[0], "selected_activity_1": selectedActivities[1], "selected_activity_2": selectedActivities[2], "selected_activity_3": nil]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count == 4 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": selectedActivities[0], "selected_activity_1": selectedActivities[1], "selected_activity_2": selectedActivities[2], "selected_activity_3": selectedActivities[3]]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        
+    }
+    
+    // Item SELECTED: update border color and save data when
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Change border color to grey
+        let SelectedCell = collectionView.cellForItem(at: indexPath)
+        SelectedCell!.layer.borderColor = UIColor.blue.cgColor
+        
+        // Create array of selected activities
+        var selectedActivities = [String?]()
+        let indexPaths = self.collectionView!.indexPathsForSelectedItems
+        for indexItem in indexPaths! {
+            let currentCell = collectionView.cellForItem(at: indexItem)! as! ActivitiesCollectionViewCell
+            let selectedActivity = currentCell.activityLabel.text
+            selectedActivities.append(selectedActivity)
+        }
+        
+        // Save array of selected activities to trip data model
+        var existing_trips = DataContainerSingleton.sharedDataContainer.usertrippreferences
+        let currentTripIndex = DataContainerSingleton.sharedDataContainer.currenttrip!
+        let tripNameValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "trip_name") as? String
+        let multipleDestionationsValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "multiple_destinations") as? String
+        let travelingInternationalValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "traveling_international") as? String
+        let suggestDestinationControlValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "suggest_destination_control") as? String
+        let suggestedDestinationValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "suggested_destination") as? String
+        let budgetValue = DataContainerSingleton.sharedDataContainer.usertrippreferences?[DataContainerSingleton.sharedDataContainer.currenttrip!].object(forKey: "budget") as? String
+        
+        if selectedActivities.count == 0 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": nil, "selected_activity_1": nil, "selected_activity_2": nil, "selected_activity_3": nil]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count == 1 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": selectedActivities[0], "selected_activity_1": nil, "selected_activity_2": nil, "selected_activity_3": nil]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count == 2 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": selectedActivities[0], "selected_activity_1": selectedActivities[1], "selected_activity_2": nil, "selected_activity_3": nil]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count == 3 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": selectedActivities[0], "selected_activity_1": selectedActivities[1], "selected_activity_2": selectedActivities[2], "selected_activity_3": nil]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count == 4 {
+            let updatedTripToBeSaved = ["trip_name": tripNameValue, "multiple_destinations": multipleDestionationsValue, "traveling_international": travelingInternationalValue, "suggest_destination_control": suggestDestinationControlValue, "suggested_destination": suggestedDestinationValue, "budget": budgetValue, "selected_activity_0": selectedActivities[0], "selected_activity_1": selectedActivities[1], "selected_activity_2": selectedActivities[2], "selected_activity_3": selectedActivities[3]]
+            existing_trips?[currentTripIndex] = updatedTripToBeSaved as NSDictionary
+            DataContainerSingleton.sharedDataContainer.usertrippreferences = existing_trips
+        }
+        else if selectedActivities.count >= 5 {
+            // Create alert with index
+            let alert = UIAlertController(title: "Please only choose 4 preferred activities", message: "Thank you", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Dismiss", style: .destructive, handler: nil)
+            alert.addAction(alertAction)
+            self.present(alert, animated: true, completion: nil)
+            
+            SelectedCell!.layer.borderColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1).cgColor
+            collectionView.deselectItem(at: indexPath, animated: true)
+        }
     }
     
     // MARK: - UICollectionViewFlowLayout
@@ -107,5 +227,4 @@ class ActivitiesViewController: UIViewController, UICollectionViewDataSource, UI
         let leftRightInset = self.view.frame.size.width / 14.0
         return UIEdgeInsetsMake(0, leftRightInset, 0, leftRightInset)
     }
-
 }
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+use_frameworks!
+
+target 'PLANiT' do
+  # Pods for PLANiT
+    pod "Koloda"
+    pod 'pop', '~> 1.0'
+
+  target 'PLANiTTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PLANiTUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
