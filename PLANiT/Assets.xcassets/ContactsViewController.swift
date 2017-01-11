82247777139ef784d111fa72f634d06ecfaeb7cc
mwurm93 <michaelwurm@michaels-macbook-pro.local>
2017-01-11 11:17:16 -0800

    mid contacts implementation

--- PLANiT/Assets.xcassets/ContactsViewController.swift
+++ PLANiT/Assets.xcassets/ContactsViewController.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
-class ContactsViewController: UIViewController  {
+class ContactsViewController: UIViewController, CNContactPickerDelegate, CNContactViewControllerDelegate  {
     
     // MARK: Outlets
     @IBOutlet weak var groupMemberListTable: UITableView!
     @IBOutlet weak var contactsSearchBar: UISearchBar!
     
+    fileprivate var addressBookStore: CNContactStore!
+    fileprivate var menuArray: NSMutableArray?
+    let picker = CNContactPickerViewController()
+
+    
+    // Contacts stuff
+//    let keysToFetch = [CNContactEmailAddressesKey, CNContactPhoneNumbersKey, CNContactFormatter.descriptorForRequiredKeys(for: .fullName)]
+//    let predicate: NSPredicate = CNContact.predicateForContacts(matchingName: "")
+//    let store = CNContactStore()
+//    let contacts = try store.unifiedContactsMatchingPredicate(CNContact.predicateForContactsMatchingName("Appleseed"), keysToFetch:[CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey])
+    
     // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        addressBookStore = CNContactStore()
+        checkContactsAccess();
+        
         groupMemberListTable.layer.cornerRadius = 5
         
         // Set appearance of search bar
@@ -34,10 +50,53 @@ class ContactsViewController: UIViewController  {
         
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    fileprivate func checkContactsAccess() {
+        switch CNContactStore.authorizationStatus(for: .contacts) {
+        // Update our UI if the user has granted access to their Contacts
+        case .authorized:
+            self.showContactsPicker()
+            
+        // Prompt the user for access to Contacts if there is no definitive answer
+        case .notDetermined :
+            self.requestContactsAccess()
+            
+        // Display a message if the user has denied or restricted access to Contacts
+        case .denied,
+             .restricted:
+            let alert = UIAlertController(title: "Privacy Warning!",
+                                          message: "Please Enable permission! in settings!.",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
+    
+    fileprivate func requestContactsAccess() {
+        
+        addressBookStore.requestAccess(for: .contacts) {granted, error in
+            if granted {
+                DispatchQueue.main.async {
+                    self.showContactsPicker()
+                    return
+                }
+            }
+        }
+    }
+    
+    //Show Contact Picker
+    fileprivate  func showContactsPicker() {
+        
+        picker.delegate = self
+        self.present(picker , animated: true, completion: nil)
+        
+    }
+    
+    
+    //    optional public func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact)
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
+
 }
